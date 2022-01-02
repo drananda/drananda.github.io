5739b76a620dc998f1fe89d86d6768a2e67c55d8
Dwiki Rizky Ananda <60393252+drananda@users.noreply.github.com>
2022-01-02 09:52:11 +0700

    Ubah selector dropdown, tambah fungsi ubah preview
    Mengubah slector untuk cv dropdown menjadi pada tag img
    Menambahkan fungsi untuk mengubah gambar dan deskripsi pada portfolio-hero dengan item portfolio yang diklik

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-const cvDropdown=document.querySelector('.header .cv-dropdown');
-const cvIconDropdown=document.querySelector('.header .cv-icon');
+cconst cvDropdown=document.querySelector('.header .cv-dropdown');
+const cvIconDropdown=document.querySelector('.header .cv-icon img');
 cvIconDropdown.addEventListener('click', function(e){
     // if(e.target.classList.contains('cv-icon')){
         cvDropdown.classList.toggle('active');
         cvIconDropdown.classList.toggle('active');
     // }
+});
+
+const portfolioHero=document.querySelector('.main-content .portfolio-hero');
+const portfolios=document.querySelectorAll('.portfolios .portfolio');
+portfolios.forEach(function(el){
+    el.addEventListener('click', function(e){
+        tmpSrc=portfolioHero.getElementsByTagName('img')[0].src;
+        portfolioHero.getElementsByTagName('img')[0].src=el.getElementsByTagName('img')[0].src;
+        el.getElementsByTagName('img')[0].src=tmpSrc;
+        tmpCaption=portfolioHero.getElementsByTagName('p')[0].innerHTML;
+        portfolioHero.getElementsByTagName('p')[0].innerHTML=el.getElementsByTagName('p')[0].innerHTML;
+        el.getElementsByTagName('p')[0].innerHTML=tmpCaption;
+    });
 });
